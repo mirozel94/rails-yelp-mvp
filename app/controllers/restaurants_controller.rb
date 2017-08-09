4e726951deef8c90939db62fb28e5a468c44e927
David Michel <davidmichel77@gmail.com>
2017-08-09 15:00:40 +0200

    done with no css so it looks awful

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,8 +12,19 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 
 end
+
+
+
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,14 +7,17 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
-    redirect_to restaurant_path(@review.restaurant_id)
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant_id)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
-
 end
+
